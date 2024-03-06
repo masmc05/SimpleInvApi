7ff43a07ab0028a0e9595926ac334c01c9d36c49
masmc05 <masmc05@gmail.com>
2024-03-06 20:12:01 +0200

    Forgot to remove the ExampleCreator.java

--- src/main/java/dev/masmc05/invapi/plugin/Plugin.java
+++ src/main/java/dev/masmc05/invapi/plugin/Plugin.java
@@ -1,15 +1,11 @@
 package dev.masmc05.invapi.plugin;
 
 import dev.masmc05.invapi.api.Api;
-import dev.masmc05.invapi.api.InventoryApi;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ServiceLoader;
 
-public class Plugin extends JavaPlugin implements Listener {
+public class Plugin extends JavaPlugin {
     @Override
     public void onEnable() {
         if (ServiceLoader.load(Api.class, Plugin.class.getClassLoader())
@@ -21,14 +17,5 @@ public class Plugin extends JavaPlugin implements Listener {
             getLogger().severe("The server version is not supported by the Inventory API. Disabling...");
             getServer().getPluginManager().disablePlugin(this);
         }
-        getServer().getPluginManager().registerEvents(this, this);
-    }
-    @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
-        // Create and open a new inventory view for the player 1 tick after joining
-        getServer().getScheduler().runTask(this, () -> {
-            ExampleCreator creator = new ExampleCreator(event.getPlayer());
-            InventoryApi.createAndOpen(event.getPlayer(), creator);
-        });
     }
 }
